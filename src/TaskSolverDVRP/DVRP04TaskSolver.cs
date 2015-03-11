5fa536f6f16be5db71674cc98edbb8af9e3f1d4a
leonczukk <leonczukk@gmail.com>
2015-03-11 11:52:09 +0100

    Added TaskSolverDVRP project.

---
+++ src/TaskSolverDVRP/DVRP04TaskSolver.cs
@@ -0,0 +1,32 @@
+﻿namespace _15pl04.UCC.TaskSolvers
+{
+    public class DVRP04TaskSolver : UCCTaskSolver.TaskSolver
+    {
+        // TODO
+
+        public DVRP04TaskSolver(byte[] problemData)
+            : base(problemData)
+        { }
+
+
+        public override byte[][] DivideProblem(int threadCount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override byte[] MergeSolution(byte[][] solutions)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override byte[] Solve(byte[] partialData, System.TimeSpan timeout)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
