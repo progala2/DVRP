db52ffaa74eacb12257fdde4031af84f4a69d6fb
Mateusz Pielat <mateusz.pielat@gmail.com>
2015-03-14 13:15:03 +0100

    Assembly info changes.

--- src/TaskSolver.Tests/Properties/AssemblyInfo.cs
+++ src/TaskSolver.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TaskSolverDVRP.Tests")]
+[assembly: AssemblyTitle("TaskSolver.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaskSolverDVRP.Tests")]
+[assembly: AssemblyProduct("TaskSolver.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/TaskSolver/Properties/AssemblyInfo.cs
+++ src/TaskSolver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TaskSolverDVRP")]
+[assembly: AssemblyTitle("TaskSolver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaskSolverDVRP")]
+[assembly: AssemblyProduct("TaskSolver")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
