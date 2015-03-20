a1f0d4e5377c86f27610df84cca3e3a2f7ecf1e9
Mateusz Pielat <mateusz.pielat@gmail.com>
2015-03-20 17:05:07 +0100

    TcpServer hotfix.

---
+++ src/Commons/TcpClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _15pl04.Ucc.Commons
+{
+    public class TcpClient
+    {
+        public TcpClient(/* ... dane serwera ... */)
+        {
+
+        }
+
+        public byte[] SendData(byte[] data)
+        {
+            /*
+             * 1. Nawiąż połączanie z serwerem.
+             * 2. Wrzuć data do bufora.
+             * 3. Oczekuj odpowiedzi od CS, zwróć ją.
+             * 
+             * Wszystko synchronicznie, a więc możliwe, że z blokowaniem.
+             * Wziąć pod uwagę backup serwery w przypadku braku odpowiedzi głównego CS.
+             */
+
+            return null;
+        }
+
+    }
+}
