ba336647bd6c459fa8b14a879e3b1154b09e90ec
progala2 <progala2@o2.pl>
2015-03-21 18:37:59 +0100

    Parser and some tests

---
+++ src/Commons.Tests/XMLParserTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Serialization;
+
+namespace _15pl04.Ucc.Commons.Tests
+{
+    [TestClass]
+    public class XMLParserTests
+    {
+        [XmlRoot("Root")]
+        public class testingClass
+        {
+            public string Child1 = "";
+            public string Child2 = "";
+        }
+        [TestMethod]
+        public void TestDeserialise()
+        {
+            XDocument doc1 = new XDocument(
+                new XElement("Root",
+                    new XElement("Child1", "content1"),
+                    new XElement("Child2", "content2")
+                )
+            );
+            var man = new Messaging.XMLParser<testingClass>();
+            testingClass tstClass = man.Deserialise(System.Text.Encoding.UTF8.GetBytes(doc1.ToString()));
+            Assert.AreEqual("content1", tstClass.Child1);
+            Assert.AreEqual("content2", tstClass.Child2);
+        }
+
+        [TestMethod]
+        public void TestSerialise()
+        {
+            XDocument doc1 = new XDocument(
+                new XElement("Root",
+                    new XElement("Child1", "content1"),
+                    new XElement("Child2", "content2")
+                )
+            );
+            var tmp = doc1.ToString();
+            byte[] buffer;
+            var man = new Messaging.XMLParser<testingClass>();
+            testingClass tstClass = new testingClass() { Child1 = "content1", Child2 = "content2" };
+            man.Serialise(tstClass, out buffer);
+            var str = System.Text.Encoding.UTF8.GetString(buffer);
+            Assert.AreEqual(tmp.Contains("<Child1>"), str.Contains("<Child1>"));
+            Assert.AreEqual(tmp.Contains("<Child2>"), str.Contains("<Child2>"));
+        }
+    }
+}
---
+++ src/Commons/Messaging/XMLParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _15pl04.Ucc.Commons.Messaging
+{
+    public class XMLParser<T>
+    {
+        public Type Type { get; set; }
+
+        public XMLParser()
+        {
+            Type = typeof(T);
+        }
+
+        public T Deserialise(byte[] buffer)
+        {
+            using (MemoryStream reader = new MemoryStream(buffer))
+            {
+                T instance;
+                XmlSerializer xml = new XmlSerializer(Type);
+                instance = (T)xml.Deserialize(reader);
+                return instance;
+            }
+        }
+
+        public void Serialise(object obj, out byte[] buffer)
+        {
+            using (MemoryStream writer = new MemoryStream())
+            {
+                XmlSerializer xml = new XmlSerializer(Type);
+                xml.Serialize(writer, obj);
+                buffer = writer.GetBuffer();
+            }
+        }
+    }
+}
