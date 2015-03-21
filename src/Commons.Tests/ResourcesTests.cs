2e014c282f6c6b0e736c6c1c00d9f3107d6ebd5f
leonczukk <leonczukk@gmail.com>
2015-03-21 12:40:09 +0100

    Added .xsd files to Resources.

---
+++ src/Commons.Tests/ResourcesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _15pl04.Ucc.Commons.Tests
+{
+    [TestClass]
+    public class ResourcesTests
+    {
+        [TestMethod]
+        public void GetManifestResourceNameReturnesExpectedString()
+        {
+            var resourcesFolder = "Resources";
+            var drf = "." + resourcesFolder + ".";
+            var assembly = Assembly.GetAssembly(typeof(Resources));
+            var expectedManifestResourceName = assembly.GetManifestResourceNames().First(s => s.Contains(drf));
+            var position = expectedManifestResourceName.LastIndexOf(drf) + drf.Length;
+            var resourceName = expectedManifestResourceName.Substring(position);
+
+            var resultManifestResourceName = Resources.GetManifestResourceName(resourceName);
+
+            Assert.AreEqual(expectedManifestResourceName, resultManifestResourceName);
+        }
+    }
+}
---
+++ src/Commons/Resources.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace _15pl04.Ucc.Commons
+{
+    public static class Resources
+    {
+        private static readonly string _resourcesFolderPath = "_15pl04.Ucc.Commons.Resources";
+
+        /// <summary>
+        /// Gets the name of resource in assembly from resource file name.
+        /// </summary>
+        /// <param name="resourceFileName">Resource file name.</param>
+        /// <returns>The name of resource in assembly.</returns>
+        public static string GetManifestResourceName(string resourceFileName)
+        {
+            return _resourcesFolderPath + "." + resourceFileName;
+        }
+    }
+}
