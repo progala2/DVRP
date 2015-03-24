1e108ceb1e5935ad614dc8a1590d7a568bf72356
Mateusz Pielat <mateusz.pielat@gmail.com>
2015-03-24 23:20:18 +0100

    Added ProblemInstance class.

--- src/Commons/Problem/ProblemInstance.cs
+++ src/Commons/Problem/ProblemInstance.cs
@@ -3,6 +3,17 @@ namespace _15pl04.Ucc.Commons.Problem
 {
     public class ProblemInstance
     {
+        public ulong Id { get; private set; }
+        public string Type { get; private set; }
+        public byte[] Data { get; private set; }
+        public ulong SolvingTimeout { get; private set; }
 
+        public ProblemInstance(ulong id, string type, byte[] data, ulong timeout = 0)
+        {
+            Id = id;
+            Type = type;
+            Data = data;
+            SolvingTimeout = timeout;
+        }
     }
 }
