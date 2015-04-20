d2a6ee8121f1d4fda78d10a03903e9be4b8a68fa
progala2 <progala2@o2.pl>
2015-04-20 10:29:23 +0200

    init version.

---
+++ src/TaskSolver/Depot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15pl04.Ucc.TaskSolver
+{
+    [Serializable]
+    public class Depot
+    {
+        public Depot(float x, float y, float startTime, float endTime)
+        {
+            X = x;
+            Y = y;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float StartTime { get; private set; }
+        public float EndTime { get; private set; }
+    }
+}
---
+++ src/TaskSolver/DvrpSolution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15pl04.Ucc.TaskSolver
+{
+    [Serializable]
+    public class DvrpSolution
+    {
+        public DvrpSolution()
+        {
+        }
+    }
+}
