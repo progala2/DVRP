7937a29809f252b2c0340b258aba652a104bc515
lavsprat <mateusz.pielat@gmail.com>
2015-05-30 15:52:13 +0200

    Last ones.

--- src/CommunicationServer/CommunicationServer.cs
+++ src/CommunicationServer/CommunicationServer.cs
@@ -7,6 +7,9 @@ using _15pl04.Ucc.CommunicationServer.WorkManagement.Base;
 
 namespace _15pl04.Ucc.CommunicationServer
 {
+    /// <summary>
+    /// Main communication server class.
+    /// </summary>
     internal class CommunicationServer
     {
         private const uint ComponentOverseerCheckInterval = 1;
@@ -15,6 +18,9 @@ namespace _15pl04.Ucc.CommunicationServer
         private readonly TcpServer _tcpServer;
         private readonly IWorkManager _workManager;
 
+        /// <summary>
+        /// </summary>
+        /// <param name="config">Server configuration.</param>
         public CommunicationServer(ServerConfig config)
         {
             // TODO Communication server should be a backup by default. Some work is needed here.
@@ -27,8 +33,14 @@ namespace _15pl04.Ucc.CommunicationServer
             _tcpServer = new TcpServer(Config.Address, _msgProcessor);
         }
 
+        /// <summary>
+        /// Server configuration.
+        /// </summary>
         public ServerConfig Config { get; private set; }
 
+        /// <summary>
+        /// Starts all modules of the server.
+        /// </summary>
         public void Start()
         {
             _tcpServer.StartListening();
@@ -36,6 +48,9 @@ namespace _15pl04.Ucc.CommunicationServer
             _componentOverseer.StartMonitoring();
         }
 
+        /// <summary>
+        /// Stops the server.
+        /// </summary>
         public void Stop()
         {
             _tcpServer.StopListening();
@@ -43,6 +58,9 @@ namespace _15pl04.Ucc.CommunicationServer
             _msgProcessor.StopProcessing();
         }
 
+        /// <summary>
+        /// Takes over the primary server functions.
+        /// </summary>
         public void SwitchToPrimaryMode()
         {
         }
