0bd804787b9ee01e7f68b751b0b2fa01b81f1633
Rad <rdk321@gmail.com>
2015-05-31 22:45:36 +0200

    Comments for commons/marshalling

--- src/Commons/Messaging/Marshalling/Base/IXmlValidator.cs
+++ src/Commons/Messaging/Marshalling/Base/IXmlValidator.cs
@@ -2,8 +2,18 @@
 
 namespace _15pl04.Ucc.Commons.Messaging.Marshalling.Base
 {
+    /// <summary>
+    /// Interface to validate with xml schema.
+    /// </summary>
+    /// <typeparam name="T">Type of schema for validation.</typeparam>
     public interface IXmlValidator<in T>
     {
+        /// <summary>
+        /// Check if document is valid with schema.
+        /// </summary>
+        /// <param name="schemaKey">Schema for checking consistency with.</param>
+        /// <param name="xml">Document to validate.</param>
+        /// <returns>Returns true if xml is valid, false otherwise.</returns>
         bool Validate(T schemaKey, XDocument xml);
     }
 }
